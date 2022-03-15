2f8e6748d10b6e75e11d6d71f820e2236ef6c5d6
reshsingh1995@gmail.com <reshsingh1995@gmail.com>
2022-03-15 23:22:11 +0530

    complete calculation

--- src/com/bridgelabz/LineComputation.java
+++ src/com/bridgelabz/LineComputation.java
@@ -89,12 +89,51 @@ public class LineComputation {
             System.out.println("Length 1 is less than Length 2");
         System.out.println(" ");
     }
+    //uc4 all three calculation in one class
+    public  void EvaluateLines() {
+        System.out.println("The Coordinates of 1st line are:");
+        System.out.print("X1 = ");
+        X1 = sc.nextInt();
+        System.out.print("Y1 = ");
+        Y1 = sc.nextInt();
+        System.out.print("X2 = ");
+        X2 = sc.nextInt();
+        System.out.print("Y2 = ");
+        Y2 = sc.nextInt();
 
+        System.out.println("The Coordinates of 2nd line are:");
+        System.out.print("X3 = ");
+        X3 = sc.nextInt();
+        System.out.print("Y3 = ");
+        Y3 = sc.nextInt();
+        System.out.print("X4 = ");
+        X4 = sc.nextInt();
+        System.out.print("Y4 = ");
+        Y4 = sc.nextInt();
+
+        Double length1 = Math.sqrt(Math.pow(X2 - X1, 2) + Math.pow(Y2 - Y1, 2));
+        System.out.println("The length of the 1st line is = " + length1);
+
+        Double length2 = Math.sqrt(Math.pow(X4 - X3, 2) + Math.pow(Y4 - Y3, 2));
+        System.out.println("The length of the 2nd line is = " + length2);
+
+        if (length1.equals(length2))
+            System.out.println("The lines are equal");
+        else if (length1>length2)
+            System.out.println("Length 1 is greater than Length 2");
+        else if (length1<length2)
+            System.out.println("Length 1 is less than Length 2");
+        else
+        System.out.println(" Lines are Not equal ");
+
+
+    }
     public static void main(String[] args){
         System.out.println("Welcome to Line Computation");
         LineComputation object = new LineComputation();
         object.DistanceBetweenPoint();
         object.equalityOfLines();
         object.comparisonBetweenLines();
+        object.EvaluateLines();
     }
 }
